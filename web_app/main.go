8dda1ab08dde0b76c95318347b260d92a66a9718
Lucas Simopoulos <lucas.simopoulos@nokia.com>
2019-04-19 11:30:18 +0000

    Add code to print

--- web_app/main.go
+++ web_app/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
 		fmt.Fprintf(w, "Hello World!")
+		fmt.Println(t.Format(time.RFC822Z), ":A new request is received")
 	})
+
+	t := time.Now()
+	fmt.Println(t.Format(time.RFC822Z), ":Server is running") 
 
 	http.ListenAndServe(":80", nil)
 }
